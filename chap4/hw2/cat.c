2bb8230311845602e4936b4e962a9004fc5d72da
JDH <jdh4568@naver.com>
2024-10-26 18:04:51 +0900

    home work commit

--- chap4/hw2/cat.c
+++ chap4/hw2/cat.c
@@ -3,13 +3,12 @@
 int main(int argc, char *argv[]) {
     FILE *fp;
     int c;
-    int line_number = 1;
-    int show_line_numbers = 0;  // 줄 번호를 출력할지 여부를 결정하는 변수
-    int file_index = 1;         // 명령줄 인수에서 파일 시작 인덱스
+    int show_line_numbers = 0;
+    int file_index = 1;
 
-    // 명령줄 인수가 없으면 표준 입력을 사용
     if (argc == 1) {
         fp = stdin;
+        int line_number = 1;
         if (show_line_numbers) {
             printf("%6d  ", line_number++);
         }
@@ -23,40 +22,34 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    // 첫 번째 인자가 '-n'이면 줄 번호를 출력
     if (argv[1][0] == '-' && argv[1][1] == 'n') {
         show_line_numbers = 1;
-        file_index = 2;  // 파일은 두 번째 인덱스부터
+        file_index = 2;
     }
 
-    // 여러 파일을 처리하는 루프
     for (int i = file_index; i < argc; i++) {
-        // 파일 열기
         fp = fopen(argv[i], "r");
         if (fp == NULL) {
-            perror("파일을 열 수 없습니다.");
-            continue;  // 다음 파일로 넘어감
+            perror("Cannot open file");
+            continue;
         }
 
-        // 첫 번째 문자를 읽는다
-        c = getc(fp);
+        int line_number = 1;
 
-        // 줄 번호가 필요하면 첫 번째 줄 번호 출력
-        if (show_line_numbers && c != EOF) {
+        if (show_line_numbers) {
             printf("%6d  ", line_number++);
         }
-
-        // EOF가 나올 때까지 한 문자씩 출력
-        while (c != EOF) {
+        while ((c = getc(fp)) != EOF) {
             putc(c, stdout);
             if (c == '\n' && show_line_numbers && !feof(fp)) {
                 printf("%6d  ", line_number++);
             }
-            c = getc(fp);
         }
-
-        // 파일 닫기
         fclose(fp);
+
+        if (i < argc - 1) {
+            printf("\n");
+        }
     }
 
     return 0;
