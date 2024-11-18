995c516b145dc6772001146cab6cf601a315a3e9
jdh4568 <jdh4568@naver.com>
2024-11-18 01:33:13 +0000

    commit

---
+++ chap10/hw1/pp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct node{
+	int data;
+	struct node *next;
+};
+void push(struct node *top, int data){
+	struct node *node = (struct node*) malloc(sizeof(struct node));
+	node -> data = data;
+	node -> next = top;
+}
+int pop(struct node *top){
+	if(top == NULL){
+		printf("under flow\n");
+		return 1;
+	}
+	struct node *node  = top;
+	int data = node -> data;
+	top = node -> next;
+	free(node);
+	return data;
+}
+
+int show(struct node *top){
+	struct node *cur = top;
+	printf("top end\n");
+	while (cur != NULL){
+		printf("%d\n", pop(cur));
+	}
+	printf("lowest top\n");
+}
+int main(){
+	int num;
+	scanf("%d", &num);
+	do{
+		scanf("%d",  &num);
+	}while(num
+
+}
